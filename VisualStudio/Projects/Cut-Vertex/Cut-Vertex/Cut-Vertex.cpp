69958e366d568fb0f581349c0e783024c062a6bd
Dabney <dabney777@outlook.com>
2016-10-16 10:35:11 +0800

    2 new Projects

---
+++ VisualStudio/Projects/Cut-Vertex/Cut-Vertex/Cut-Vertex.cpp
@@ -0,0 +1,109 @@
+﻿// Cut-Vertex.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+#include <fstream>
+#define MAXNODE 100
+using namespace std;
+
+//adjacency list for undirected graph
+typedef struct ArcNode {									
+	int adjvex;
+	ArcNode* nextarc;
+}ArcNode;
+
+typedef struct VNode {
+	string data;
+	bool visted;
+	bool isCut = false;
+	ArcNode* firstarc;
+}VNode;
+
+typedef struct GraphAdjcent{											
+	VNode adjList[MAXNODE];
+	int nodesNum, arcsNum;
+}GraphAdjcent;
+
+GraphAdjcent adjGraph;									//adjcentlist is adjgraph.adjList
+
+void CreatGraph() {
+	int nodesNum, arcsNum;
+	cout << "please input nodes Num:" << endl;
+	cin >> nodesNum;
+	cout << "please input arcsNum:" << endl;
+	cin >> arcsNum;
+	
+	adjGraph.nodesNum = nodesNum;
+	adjGraph.arcsNum = arcsNum;
+	for (int i = 0; i < adjGraph.nodesNum; ++i) {		//initialize all Node
+		adjGraph.adjList[i].firstarc = NULL;
+
+	}
+	
+	cout << "plese input nodes information:(here is empty ,do not entry anything now)" << endl;
+	for (int i = 0; i < adjGraph.nodesNum; ++i) {
+		//cin >> adjcentlist.AdjList[i].data;
+		
+	}
+	cout << "please input arcs informantion using formula like v1,v2:" << endl;
+	for (int i = 0; i < adjGraph.arcsNum; ++i) {
+		if(i!=0) cout << "then:" << endl;
+		cout << ">>";
+		int j, k;
+		ifstream in("C:\\Users\\Dabney\\Desktop\\graphdataSet.txt");
+		//scanf_s("%d,%d",&j,&k);
+		
+		ArcNode* arcj = (ArcNode*)malloc(sizeof(ArcNode));
+		arcj->adjvex = k;
+		arcj->nextarc = adjGraph.adjList[j].firstarc;
+		adjGraph.adjList[j].firstarc = arcj;					//vertex[i] has been added edge
+
+		ArcNode* arck = (ArcNode*)malloc(sizeof(ArcNode));
+		arck->adjvex = j;
+		arck->nextarc = adjGraph.adjList[k].firstarc;
+		adjGraph.adjList[k].firstarc = arck;					//vertex[k] hase been added edge
+	}
+}
+
+int DFScount=0;
+int visted[MAXNODE];
+int low[MAXNODE];
+void DFS(GraphAdjcent &g,int v) {
+	if (g.adjList[v].firstarc == NULL|| g.adjList[v].visted) return;
+	g.adjList[v].visted = true;
+	visted[v] = DFScount++;	
+	low[v] = visted[v];
+	ArcNode* arc = g.adjList[v].firstarc;
+	for(;arc!=NULL;arc=arc->nextarc)	DFS(g, arc->adjvex);
+	arc = g.adjList[v].firstarc;				//initialize arc for search minimum count
+	for (; arc != NULL; arc = arc->nextarc) {
+		if (low[arc->adjvex] < low[v]) low[v] = low[arc->adjvex];
+	}
+	arc = g.adjList[v].firstarc;				//initialize arc for search cut vertex
+	for (; arc != NULL; arc = arc->nextarc) {
+		if (low[arc->adjvex] >= visted[v])  g.adjList[v].isCut = true; 
+	}
+	
+}
+void findCut(GraphAdjcent g, int v,int c[]) {
+	DFS(g, v);
+	for (int i = 0; i < g.nodesNum; ++i) {
+		if (g.adjList[i].isCut) c[i]++;
+	}
+}
+
+int main()
+{
+	CreatGraph();
+	int cutCount[MAXNODE] = { 0 };
+	findCut(adjGraph, 0,cutCount);
+	findCut(adjGraph, 1,cutCount);
+	for (int i = 0; i < adjGraph.nodesNum; ++i) {
+		if (cutCount[i]>1) cout<<"The node ["<<i<<"] is a cut-vertex!"<<endl;
+	}
+
+	system("PAUSE");
+    return 0;
+}
---
+++ VisualStudio/Projects/TestingBipartiteness/TestingBipartiteness/TestingBipartiteness.cpp
@@ -0,0 +1,122 @@
+// TestingBipartiteness.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<fstream>
+#include<iostream>
+#include<string>
+
+#define MAXNODE 100 
+#define MAXARC 200
+
+
+using namespace std;
+
+typedef struct ArcNode {
+	int adjvex = 0;
+	ArcNode* next=NULL;
+}ArcNode;
+
+typedef struct VNode {
+	string data;
+	int color;//0 is blue,1 is red
+	ArcNode* fistArc = NULL;
+}VNode;
+
+typedef struct AdjcentGraph {
+	VNode node[MAXNODE];
+	ArcNode arc[MAXARC];
+	int numOfNode = 0, numOfArc = 0;
+}AdjcentGraph;
+AdjcentGraph graph;
+
+//int blue[MAXNODE], red[MAXNODE];
+
+						/*Data format: [numofnode] [numofarc] 
+					 	 *[information about node]						   | here is no information
+					 	 *[information about arc:start vertex ,end vertex] | no square bracket*/
+
+void creatGraph() {
+	ifstream in("C:\\Users\\Dabney\\Documents\\WorkPlace\\TestCase\\Bipartiteness.txt");
+	in >> graph.numOfNode >> graph.numOfArc;
+	for (int i = 0; i < graph.numOfNode; i++) {
+		graph.node[i].fistArc = NULL;
+		graph.node[i].color = 2;
+	}
+	for (int i = 0; i < graph.numOfArc; i++) {
+		int head=0, tail=0;
+		in >> head >> tail;
+		ArcNode *arc = (ArcNode*)malloc(sizeof(ArcNode));
+		arc->next = graph.node[head].fistArc;
+		graph.node[head].fistArc = arc;
+		arc->adjvex = tail;
+		arc = (ArcNode*)malloc(sizeof(ArcNode));
+		arc->next = graph.node[tail].fistArc;
+		graph.node[tail].fistArc = arc;
+		arc->adjvex = head;
+	}
+}
+bool visted[MAXNODE] = { false };
+
+void BFS(AdjcentGraph &g, int start) {
+	VNode *queue[MAXNODE];
+	queue[0] = &g.node[start];
+	int tail = 1, head = 0;		//tail point to an empty postion
+	
+	queue[0]->color = 0;
+	while (tail > head) { 
+		ArcNode *tmp;
+		tmp = queue[head]->fistArc;
+		while (tmp != NULL) {
+			if (visted[tmp->adjvex] == true) {			//is bipartiteness?
+				switch (queue[head]->color) {
+				case 0:if (g.node[tmp->adjvex].color == 0) {
+					cout << "this is not a bipartitioness graph!" << endl;
+					return;
+				}; break;
+				case 1:if (g.node[tmp->adjvex].color == 1) {
+					cout << "this is not a bipartitioness graph!" << endl;
+					return;
+				}; break;
+				default:cout << "There is a mistake" << endl;
+				}
+			}
+			if (visted[tmp->adjvex] == false) {
+				visted[tmp->adjvex] = true;
+				switch (queue[head]->color) {
+					case 0:g.node[tmp->adjvex].color = 1; break;
+					case 1:g.node[tmp->adjvex].color = 0; break;
+					default:cout << "There is a mistake" << endl;
+					}
+				queue[tail++] = &g.node[tmp->adjvex];
+			}
+			tmp = tmp->next;
+		}
+		head++;
+	}	
+}
+
+
+
+int main()
+{
+	creatGraph();
+	for (int i = 0; i < graph.numOfNode ;i++) {
+		if(visted[i]==false)
+		BFS(graph, i);
+	}
+	string color;
+	for (int i = 0; i < graph.numOfNode; i++) {
+		switch (graph.node[i].color){
+		case 0:color = "Blue"; break;
+		case 1:color = "Red"; break;
+		default:cout << "i am not a bipartiteness graph!";
+			system("PAUSE");
+			return 0;
+		}
+		cout <<i << " belong to " << color << endl;
+	}
+	system("PAUSE");
+    return 0;
+}
+
